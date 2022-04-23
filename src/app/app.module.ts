5fd38ae78a75d11d481604522927f335452b50fb
Aldanan13 <aldananiscola@gmail.com>
2022-04-23 16:25:47 -0300

    foot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { RedesComponent } from './components/redes/redes.component';
 import { LogComponent } from './components/log/log.component';
 import { BannerComponent } from './components/banner/banner.component';
-
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaLaboralComponent } from './components/experiencia-laboral/experiencia-laboral.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +22,18 @@ import { BannerComponent } from './components/banner/banner.component';
     RedesComponent,
     LogComponent,
     BannerComponent,
+    AcercaDeComponent,
+    ExperienciaLaboralComponent,
+    EducacionComponent,
+    HysComponent,
+    ProyectoComponent,
+    FooterComponent,
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
