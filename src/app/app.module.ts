234b64c439a59d968a7d63c1daabcdce1600fa44
Aldanan13 <aldananiscola@gmail.com>
2022-04-24 11:11:06 -0300

    componentesTerminados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { RedesComponent } from './components/redes/redes.component';
 import { LogComponent } from './components/log/log.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
@@ -19,7 +18,6 @@ import { FooterComponent } from './components/footer/footer.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    RedesComponent,
     LogComponent,
     BannerComponent,
     AcercaDeComponent,
